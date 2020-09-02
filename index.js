9de4fbb889e6d251c48dcb006bb3cb4c7a4b6af0
Gentlesammy <samodunlade84@gmail.com>
2020-09-02 07:43:58 -1100

    Basic routing added and about page with 404 page

--- index.js
+++ index.js
@@ -84,9 +84,22 @@ const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
   // set header
   res.setHeader("Content-Type", "text/html");
+  // simple routing begins, set default path and switch based on what is in url
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
   //   res.write("<h1>Hello Ninja</h1>");
   //   res.write("<p>It is a new begining</p>");
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
