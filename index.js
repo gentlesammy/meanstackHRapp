8c47f2b89f21d5515cef75af2d092b3fe7add843
Gentlesammy <samodunlade84@gmail.com>
2020-09-02 07:00:07 -1100

    serving view from external file

--- index.js
+++ index.js
@@ -79,14 +79,22 @@ working with HTTP
 
 */
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
   // set header
   res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello Ninja</h1>");
-  res.write("<p>It is a new begining</p>");
-  res.end();
+  //   res.write("<h1>Hello Ninja</h1>");
+  //   res.write("<p>It is a new begining</p>");
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //   res.write(data); when sending one thing we dont need this
+      res.end(data);
+    }
+  });
 });
 
 server.listen(5000, "localhost", () => {
