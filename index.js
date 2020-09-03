c5ff20c3dc29cc936b5f8c3cf8285220bf51f7f5
Gentlesammy <samodunlade84@gmail.com>
2020-09-02 18:50:35 -1100

    Express routing used for sending html file

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World</h1>");
+  // res.send("<h1>HomePage</h1>");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", function (req, res) {
+  // res.send("<h1>HomePage</h1>");
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+//redirect
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+//404
+app.use((req, res) => {
+  res.sendFile("./views/404.html", { root: __dirname });
 });
 
 app.listen(5000);
