00cb533b3c87ed2dde66042ebe28984ffec5aa91
Gentlesammy <samodunlade84@gmail.com>
2020-09-02 21:05:49 -1100

    ejs templating added with static display

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
 
+//routes
 app.get("/", function (req, res) {
   // res.send("<h1>HomePage</h1>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Welcome" });
 });
 
 app.get("/about", function (req, res) {
   // res.send("<h1>HomePage</h1>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About Us" });
 });
 
-//redirect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/create", function (req, res) {
+  // res.send("<h1>HomePage</h1>");
+  res.render("create", { title: "submit report" });
+});
+
+app.get("/honestlist", function (req, res) {
+  // res.send("<h1>HomePage</h1>");
+  res.render("honestlist", { title: "Honest List" });
+});
+
+// app.get("/about", function (req, res) {
+//   // res.send("<h1>HomePage</h1>");
+//   res.render("about", { title: "About Us" });
+// });
+
+app.get("/terms", function (req, res) {
+  // res.send("<h1>HomePage</h1>");
+  res.render("terms", { title: "terms and condition" });
 });
 
 //404
 app.use((req, res) => {
-  res.sendFile("./views/404.html", { root: __dirname });
+  // res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
 
 app.listen(5000);
